f5839c2419f78c61ff0e4ff88ddfc628d0036ef5
TaratipT <66070086@kmitl.ac.th>
2025-09-16 15:02:39 +0700

    change to while loop

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,7 +1,8 @@
 num = int(input("Enter a number less than 25:\n"))
 
-if num < 25:
-    for i in range(num, 26):
-        print("Inside the loop. my variable is", i)
-else:
+if num > 25:
     print("Error")
+else:
+    while num <= 25:
+        print("Inside the loop. my variable is", num)
+        num += 1
